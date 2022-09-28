10e7d4c41621b0337f1959100018cf85104787a9
Mostafa Sayed <promostafa82@gmail.com>
2022-09-28 18:52:06 +0200

    Modify Shipping Address on Order Model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -21,10 +21,13 @@ const orderSchema = mongoose.Schema(
 			},
 		],
 		shippingAddress: {
+			firstName: { type: String, required: true },
+			lastName: { type: String, required: true },
 			address: { type: String, required: true },
 			city: { type: String, required: true },
 			postalCode: { type: String, required: true },
 			country: { type: String, required: true },
+			region: String,
 		},
 		paymentMethod: {
 			type: String,
