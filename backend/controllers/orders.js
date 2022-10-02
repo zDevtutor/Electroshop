310c79ce067e848c9767478f37f91f95be401feb
Mostafa Sayed <promostafa82@gmail.com>
2022-10-02 16:46:41 +0200

    Change if/else statment to be only if statment

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -14,34 +14,31 @@ exports.addOrder = asyncHandler(async (req, res) => {
 		totalPrice,
 	} = req.body;
 
-	if (orderItems && orderItems === 0) {
+	if (orderItems && orderItems.length === 0) {
 		res.status(401);
 		throw new Error('No Order Items');
-	} else {
-		const order = new Order({
-			user: req.user._id,
-			orderItems,
-			shippingAddress,
-			paymentMethod,
-			taxPrice,
-			shippingPrice,
-			totalPrice,
-		});
-
-		const createdOrder = await order.save();
-
-		res.status(201).json(createdOrder);
 	}
+
+	const order = new Order({
+		user: req.user._id,
+		orderItems,
+		shippingAddress,
+		paymentMethod,
+		taxPrice,
+		shippingPrice,
+		totalPrice,
+	});
+
+	const createdOrder = await order.save();
+
+	res.status(201).json(createdOrder);
 });
 
 // @desc    Get Order By Id
 // @route   GET /api/orders/:id
 // @access  Private
 exports.getOrder = asyncHandler(async (req, res) => {
-	const order = await Order.findById(req.params.id).populate(
-		'user',
-		'name email'
-	);
+	const order = await Order.findById(req.params.id).populate('user', 'email');
 
 	if (!order) {
 		res.status(404);
