49f09bb297c22e5044b90c9d765938ea8ea5d318
Mostafa Sayed <promostafa82@gmail.com>
2022-10-11 14:49:57 +0200

    Fix issue of loading backend files instead of frontend

--- backend/app.js
+++ backend/app.js
@@ -16,23 +16,19 @@ const app = express();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+app.use('/api/products', productsRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/uploads', uploadRoute);
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 	});
-} else {
-	app.get('/', (req, res) => {
-		res.send('App is Running');
-	});
 }
-
-app.use('/api/products', productsRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/users', usersRoute);
-app.use('/api/orders', orderRoute);
-app.use('/api/uploads', uploadRoute);
 
 app.use(notFound);
 app.use(errorHandler);
