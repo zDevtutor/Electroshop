71c2131a10c08f55652651fced0b06d4aa2eaec6
Mostafa Sayed <promostafa82@gmail.com>
2023-10-22 15:50:45 +0300

    change mongoose uri to string

--- backend/app.js
+++ backend/app.js
@@ -37,10 +37,13 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+	.connect(
+		'mongodb+srv://mos123:sd2YB1hFsJQKdPnE@cluster0.xa1zvfe.mongodb.net/electroshop?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
 	.then(() => {
 		app.listen(PORT, console.log(`Server Is Running On Port ${PORT}`));
 	})
