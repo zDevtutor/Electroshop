c080a32e17caa8dd7050ba1efaae2e35eb0040d8
Mostafa Sayed <promostafa82@gmail.com>
2023-10-22 15:43:34 +0300

    create build folder and fix mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,10 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.MONGO_URI)
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() => {
 		app.listen(PORT, console.log(`Server Is Running On Port ${PORT}`));
 	})
