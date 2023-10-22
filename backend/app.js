1203e64e4cc7b29f725ea03abe444cde50dc661e
Mostafa Sayed <promostafa82@gmail.com>
2023-10-22 15:57:36 +0300

    Reverse back to enviorment variable on mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -37,13 +37,10 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(
-		'mongodb+srv://mos123:sd2YB1hFsJQKdPnE@cluster0.xa1zvfe.mongodb.net/electroshop?retryWrites=true&w=majority',
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}
-	)
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() => {
 		app.listen(PORT, console.log(`Server Is Running On Port ${PORT}`));
 	})
